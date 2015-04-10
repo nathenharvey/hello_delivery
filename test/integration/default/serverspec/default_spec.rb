22df14493b352607fc68171cb7dedf75de9cf534
nathenharvey <nharvey@chef.io>
2015-04-10 01:57:47 -0400

    correct rubocop offenses

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -4,7 +4,7 @@ describe 'hello_delivery::default' do
 
   # Serverspec examples can be found at
   # http://serverspec.org/resource_types.html
-  
+
   it 'does something' do
     skip 'Replace this with meaningful tests'
   end
